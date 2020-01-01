7f1b895f36864cdc6f4244a79136a524cb01229f
lakshitha perera <lakshitha1629@gmail.com>
2020-01-01 23:51:53 +0530

    Update System

--- common/footer.php
+++ common/footer.php
@@ -74,23 +74,23 @@
                     <form action="#">
                         <div class="form-group">
                             <!-- <label for="appointment_name" class="text-black">Full Name</label> -->
-                            <input type="text" class="form-control" id="appointment_name" placeholder="Full Name">
+                            <input type="text" class="form-control" placeholder="Full Name">
                         </div>
                         <div class="form-group">
                             <!-- <label for="appointment_email" class="text-black">Email</label> -->
-                            <input type="text" class="form-control" id="appointment_email" placeholder="Email">
+                            <input type="text" class="form-control" placeholder="Email">
                         </div>
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <!-- <label for="appointment_date" class="text-black">Date</label> -->
-                                    <input type="text" class="form-control" id="appointment_date" placeholder="Date">
+                                    <input type="text" class="form-control" placeholder="Date">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <!-- <label for="appointment_time" class="text-black">Time</label> -->
-                                    <input type="text" class="form-control" id="appointment_time" placeholder="Time">
+                                    <input type="text" class="form-control" placeholder="Time">
                                 </div>
                             </div>
                         </div>
@@ -98,7 +98,7 @@
 
                         <div class="form-group">
                             <!-- <label for="appointment_message" class="text-black">Message</label> -->
-                            <textarea name="" id="appointment_message" class="form-control" cols="30" rows="10" placeholder="Message"></textarea>
+                            <textarea name="" class="form-control" cols="30" rows="10" placeholder="Message"></textarea>
                         </div>
                         <div class="form-group">
                             <input type="submit" value="Get a Quote" class="btn btn-primary">
